bb918e2422335e04411345773f9a9de6cf64531b
Lakshmi00123 <lakshmibandreddi19@gmail.com>
2025-06-26 14:06:43 +0530

    Hello

---
+++ src/org/greens/string/Length.java
@@ -0,0 +1,27 @@
+package org.greens.string;
+
+public class Length {
+	public static void main(String[] args) {
+		String s = "Greens Technology";
+		System.out.println(s);
+		int length = s.length();
+		System.out.println(length);
+		String s1 = "SeleniumAutomationtool";
+		System.out.println(s1);
+		int length1 = s1.length();
+		System.out.println(length1);
+		String s2 = "velmurugan";
+		System.out.println(s2);
+		int length2 = s2.length();
+		System.out.println(length2);
+		String s3 = "j a v a p r o g r a m";
+		System.out.println(s3);
+		int length3 = s3.length();
+		System.out.println(length3);
+		String s4 = "9944152058";
+		System.out.println(s4);
+		int length4 = s4.length();
+		System.out.println(length4);
+
+	}
+}
